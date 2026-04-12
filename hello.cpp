8082a945c79c55e36df8ce7c2e0e0a40ac4a6063
Nawal Fatima <nawalfatima207@gmail.com>
2026-04-13 01:46:58 +0500

    check

--- hello.cpp
+++ hello.cpp
@@ -2,8 +2,7 @@
 using namespace std;
 int main()
 {
-   cout << " checking push and pull statements " << endl;
-    cout<<"Hello World!"<<endl; 
+    cout << "we are going to try something new in git" << endl;
     cout << "testing to push code in second branch" << endl;
     cout <<"nw branch code added" << endl;
     return 0;
