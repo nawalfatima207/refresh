84b8e020884ec170b43f84d5d1e47d1d07d993ac
Nawal Fatima <nawalfatima207@gmail.com>
2026-04-13 01:52:57 +0500

    Change 2

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,6 @@ int main()
 {
     cout << "we are going to try something new in git" << endl;
     cout << "testing to push code in second branch" << endl;
-    cout <<"nw branch code added" << endl;
+    cout <<"change is happening to check merge" << endl;
     return 0;
 }
