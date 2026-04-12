1e9fbc6472295a71a137bb05243225c4daa078ac
Nawal Fatima <nawalfatima207@gmail.com>
2026-04-13 01:59:36 +0500

    new

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,8 @@
 #include<iostream>
 using namespace std;
 int main()
-{
-    cout << "we are going to try something new in git" << endl;
-    cout << "testing to push code in second branch" << endl;
+{ int X,Y;
+    cin >> X>>Y;
     cout <<"change is happening to check merge" << endl;
     return 0;
 }
